c78069c53bb4d329a7fca83c7494c6675a03dc8f
Suriyadeepan Ram <suriyadeepan@acm.org>
2015-09-19 12:19:55 +0530

    cjdns interface complete : provides a vector of ipv6-peers

--- src/cjdns/cjdns.cc
+++ src/cjdns/cjdns.cc
@@ -1,5 +1,9 @@
 #include "stdio.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
 
 int main(){
 
@@ -8,14 +12,17 @@ int main(){
 	int status;
 	char path[100];
 
+	vector<string> ip_list;
 
-	fp = popen("$HOME/install/cjdns/tools/dumptable | grep v16", "r");
+	fp = popen("./loop_dumptable","r");
+	//fp = popen("$HOME/install/cjdns/tools/dumptable");
 	if (fp == NULL)
 		/* Handle error */;
 
 
 	while (fgets(path, 100, fp) != NULL){
-			printf("%s\n", path);
+			//printf("%s", path);
+			ip_list.push_back(path);
 	}
 
 
@@ -26,5 +33,13 @@ int main(){
 		/* Use macros described under wait() to inspect `status' in order
 			 to determine success/failure of command executed by popen() */
 	}
+
+
+	printf("-----------------");
+	for(int i=0;i<ip_list.size();i++)
+		printf("\n%s",ip_list[i].c_str());
+	printf("-----------------");
+
+	return 0;
 
 }
---
+++ src/model/model.h
@@ -0,0 +1,3 @@
+#define MODEL_H
+
+
