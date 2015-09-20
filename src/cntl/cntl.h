a0bf7ce27de1f708f601de62c7594a9444091287
Suriyadeepan Ram <suriyadeepan@acm.org>
2015-09-20 07:21:12 +0530

    add comments on message format to cntl/ module

--- src/cntl/cntl.h
+++ src/cntl/cntl.h
@@ -3,7 +3,18 @@
 
 #include "../cjdchat.h"
 
+/* -- Moved to ../cjdchat.h -- */
 //extern enum {JOIN,MSG,PART,PARTALL,IGNORE,QUIT,HELP};
+/*
+ *  Message format:
+		1. "blah blah" -> normal msg (broadcasted to all)
+		2. "/msg nick ksjdf" -> private msg, unicasted to ipv6(nick)
+		3. "/part chan" -> leave channel
+		4. "/partall" -> leave all channels except #general
+		5. "/join chan" -> join chan
+		6. "/quit" -> exit
+
+ */
 
 //int cntl_identify(char *);
 int cntl_splitter(char* ,char* );
